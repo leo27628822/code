2f3021ee69ea79161df61e6b6c0032cd69af9dbe
Sakawood <leo27628822@gmail.com>
2021-10-08 19:16:51 +0800

    111

--- C++/DS作業/練習02/DS1101ex02_10927143.cpp
+++ C++/DS作業/練習02/DS1101ex02_10927143.cpp
@@ -14,7 +14,7 @@ public:
     vector< vector<char> > v1 ;
     vector< char > v2 ;
     void init() ;
-    void print() {  // 運用迭代遍歷
+    void print() {  // 運用迭代器遍歷
         vector< vector<char> >::iterator it2 = v1.begin() ;
         while ( it2 != v1.end() ) {
             vector< char >::iterator it = (*it2 ).begin() ;
@@ -28,11 +28,9 @@ public:
         } // while
     } // print()
 
-    void Search() ;
-    bool isPath_() ;
-
-private:
-
+    void Search( int i, int j, bool & find ) ;
+    void SearchR( int i, int j, bool & find ) ;
+    void Search_For_N( int N, int i, int j, bool & find) ;
 
 };
 
@@ -43,7 +41,7 @@ void Maze::init() {
     stringstream ss ;
     string filename = "input" ;
     cin >> num ;
-    ss << filename << num ; // 將字串和數字一起丟進去字串流
+    ss << filename << num ;        // 將字串和數字一起丟進去字串流
     ss >> filename ;               // 將流內的字串丟回 filename
     filename += ".txt" ;           // 之後再字串結尾加上 .txt
     cout << filename << endl ;
@@ -52,7 +50,7 @@ void Maze::init() {
         cout << "Failed to open file.\n";
     } // if
     else {
-        ifs >> x >> y ;               // x : 列 row  || y : 行 column
+        ifs >> x >> y ;            // x : 列 row  || y : 行 column
         for ( int i = 0 ; i < y ; i++ ) {
             for ( int j = 0 ; j < x ; j++ ) {
                 char ch ;
@@ -69,63 +67,63 @@ void Maze::init() {
 
 } // init()
 
+
+void Maze::Search( int i, int j, bool & find) {
+
+    if ( i < 0 || j < 0 || j >= x || i >= y ||  !( v1[i][j] == 'E' || v1[i][j] == 'G' ) ) return ; // 避免超過vector的大小
+    if ( v1[i][j] == 'G' ) { // check goal reached?
+        find = true ;
+        return ;
+    } // if()
+
+    v1[i][j] = 'V' ;    // mark visited
+    if ( !find ) Search( i, j+1, find ) ;  // go right
+    if ( !find ) Search( i, j-1, find ) ;  // go left
+    if ( !find ) Search( i+1, j, find ) ;  // go down
+    if ( !find ) Search( i-1, j, find ) ;  // go up 
+    
+} // Search()
+
+void Maze::SearchR( int i, int j, bool & find) {
+
+    if ( i < 0 || j < 0 || j >= x || i >= y ||  !( v1[i][j] == 'E' || v1[i][j] == 'G' ) ) return ; // 避免超過vector的大小
+    if ( v1[i][j] == 'G' ) { // check goal reached?
+        find = true ;
+        return ;
+    } // if()
+
+    v1[i][j] = 'R' ;    // mark route
+    if ( !find ) SearchR( i, j+1, find ) ;  // go right
+    if ( !find ) SearchR( i, j-1, find ) ;  // go left
+    if ( !find ) SearchR( i+1, j, find ) ;  // go down
+    if ( !find ) SearchR( i-1, j, find ) ;  // go up 
+    if ( !find ) v1[i][j] = 'E' ;
+    
+} // SearchR()
+
+void Maze::Search_For_N( int N, int i, int j, bool & find) {
+    return ;
+} // Search_For_N()
+
 int main() {
 
     cout << "0. Quit || 1. One goal || 2. More goals\n" ;
     int command ;
+    bool find = false ;
     // 0: quit 1: one goal 2: more goals
     while ( cin >> command && command ) {
         Maze mouse ;
         mouse.init() ;
         mouse.print() ;
+        cout << endl ;
+        mouse.Search( 0,0, find ) ;
+        mouse.print() ;
+        cout << endl ;
         cout << "0. Quit || 1. One goal || 2. More goals\n" ;
-    } // while
-
-        /*
-        cout << "Input a file number : ( e.g., 201, 202, ...... ) : " ;
-        int num ;
-        stringstream ss ;
-        string filename = "input" ;
-        cin >> num ;
-        ss << filename << num ; // 將字串和數字一起丟進去字串流
-        ss >> filename ;               // 將流內的字串丟回 filename
-        filename += ".txt" ;           // 之後再字串結尾加上 .txt
-        cout << filename << endl ;
-        ifstream ifs( filename, std::ios::in);
-        if ( !ifs.is_open() ) {
-            cout << "Failed to open file.\n";
-        } // if
-        else {
-            ifs >> mouse.x >> mouse.y ;
-            mouse.init( ifs, mouse.x, mouse.y ) ;
-            ifs.close() ;
-        } // else
-
-        ifs.close() ;
-        ss.str( "" ) ;
-        ss.clear() ;
+        find = false ;
     } // while
 
     system( "pause" ) ;
-    /*
-    stringstream ss ;
-    ifstream ifs("input201.txt", std::ios::in);
-    if ( !ifs.is_open() ) {
-        cout << "Failed to open file.\n";
-    } // if
-    else {
-        int x,y ;
-        ifs >> x >> y ;
-        for ( int i = 0 ; i < x ; i++ )
-        cout << x << y << endl ;
-        ifs.close() ;
-    }
-    ifs.close() ;
 
-    ss.str( "" ) ;
-    ss.clear() ;
-    return 0;
-    */
-
-}
+} // main()
 
