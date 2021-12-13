5f2425765ac824dce8bf4975e52bb909f81daff5
Sakawood <leo27628822@gmail.com>
2021-12-13 22:40:17 +0800

    1515

--- C++/DS作業/練習05/DS1101ex5_20_10927143_10927141.cpp
+++ C++/DS作業/練習05/DS1101ex5_20_10927143_10927141.cpp
@@ -35,7 +35,8 @@ class Tree {
         } // end Tree
 
         ~Tree() {
-
+            delete root ;
+            root = NULL ;
         } // end ~Tree
 
         void createTree1( vector<Node> tree ) ;
@@ -57,10 +58,12 @@ class Tree {
         void init() {
             root = NULL ;
         } // end init
+        void search_name( string name, TreeNode * tempPtr, int & i, bool & find ) ;
+        void search_graduate( int number, TreeNode * tempPtr, int & i, bool & find ) ;
 
 };
 
-Tree::getHeight( TreeNode * temp ) {
+int Tree::getHeight( TreeNode * temp ) {
     int lh, rh ;
     if ( !temp ) return 0 ;
     else {
@@ -69,7 +72,6 @@ Tree::getHeight( TreeNode * temp ) {
         return  lh > rh ? lh+1 : rh+1 ;
     } // end else
 } // end getHeight
-
 
 void Tree::createTree1( vector<Node> tree ) {
     for ( int i = 0 ; i < tree.size() ; i++ ) {
@@ -127,6 +129,36 @@ void Tree::createTree2( vector<Node> tree ) {
     } // end for
 } // end createTree2
 
+void Tree::search_name( string name, TreeNode * tempPtr, int & i, bool & find ) {
+    if ( tempPtr == NULL ) return ;
+    if ( tempPtr -> node.school == name ) {
+        if ( i == 0 ) cout << "\n" << title << "\n" ;
+        find = true ;
+        cout << "[" << i+1 << "]\t" << tempPtr -> node.school << "\t" << tempPtr -> node.major << "\t" ;
+        cout << tempPtr -> node.DN_en << " " << tempPtr -> node.DN_ch << "\t" << tempPtr -> node.RK_en << " " ;
+        cout << tempPtr -> node.RK_ch << "\t" << tempPtr -> node.student << "\t" << tempPtr -> node.teacher << "\t" ;
+        cout << tempPtr -> node.graduate << "\n" ;
+        i++ ;
+    } // end if
+    if ( name < tempPtr -> node.school ) search_name( name, tempPtr -> left, i, find ) ;
+    else search_name( name, tempPtr -> right, i, find ) ;
+} // end search_name
+
+void Tree::search_graduate( int number, TreeNode * tempPtr, int & i, bool & find ) {
+    if ( tempPtr == NULL ) return ;
+    if ( tempPtr -> node.graduate >= number ) {
+        if ( i == 0 ) cout << "\n" << title << "\n" ;
+        find = true ;
+        cout << "[" << i+1 << "]\t" << tempPtr -> node.school << "\t" << tempPtr -> node.major << "\t" ;
+        cout << tempPtr -> node.DN_en << " " << tempPtr -> node.DN_ch << "\t" << tempPtr -> node.RK_en << " " ;
+        cout << tempPtr -> node.RK_ch << "\t" << tempPtr -> node.student << "\t" << tempPtr -> node.teacher << "\t" ;
+        cout << tempPtr -> node.graduate << "\n" ;
+        i++ ;
+        search_graduate( number, tempPtr -> left, i, find ) ;
+    } // end if
+    search_graduate( number, tempPtr -> right, i, find ) ;
+} // end search_name
+
 bool Read_File( vector <Node> & data ) {
     int tempint ;
     string filename, tempstr ;
@@ -176,10 +208,12 @@ int main() {
     vector <Node> data ;
     PrintTitle() ;
     Tree tree1, tree2 ;
+    bool check = false, find = false ;
     while ( cin >> command && command ) {
 
         cin.ignore() ;
         if ( command == 1 && Read_File( data ) ) {
+            check = true ;
             tree1.init() ;
             tree2.init() ;
             cout << title << "\n" ;
@@ -190,23 +224,40 @@ int main() {
                 cout << data[i].graduate << "\n" ;
             } // end for
 
-
             tree1.createTree1( data ) ;
             tree2.createTree2( data ) ;
             cout << "\nTree height {School name} = " << tree1.Height() << "\n" ;
             cout << "\nTree height {Number of graduates} = " << tree2.Height() << "\n" ;
-
         } // end if
         else if ( command == 2 ) {
-            cout << ;
-            cin >> ;
+            if ( !check ) cout << "\nPlease choose command 1 first!\n" ;
+            else {
+                int number ;
+                cout << "Input the number of graduates:" ;
+                while ( cin >> number && number < 0 ) {
+                    cout << "\n### the input number " << number << " is not a decimal number! ###\n" ;
+                    cout << "Input the number of graduates:" ;
+                } // end while
+                cin.ignore() ;
+                int i = 0 ;
+                tree2.search_graduate( number, tree2.getRoot(), i, find ) ;
+                if ( !find ) cout << "There is no match!\n" ;
+            } // end else
         } // end else if
         else if ( command == 3 ) {
-            cout << ;
-            cin >> ;
+            if ( !check ) cout << "\nPlease choose command 1 first!\n" ;
+            else {
+                string name ;
+                cout << "Input a school name:" ;
+                getline( cin, name ) ;
+                int i = 0 ;
+                tree1.search_name( name, tree1.getRoot(), i, find ) ;
+                if ( !find ) cout << "There is no match!\n" ;
+            } // end else
         } // end else if
         else cout << "\nCommand does not exist!\n" ;
         data.clear() ;
+        find = false ;
         cout << "\n" ;
         PrintTitle() ;
 
