a244294f287b48168b3775ba4b159bb751723128
Sakawood <leo27628822@gmail.com>
2022-01-04 00:00:37 +0800

    010

--- Python/工數作業2/test.py
+++ Python/工數作業2/test.py
@@ -6,15 +6,15 @@ from math import factorial
 import sympy
 
 def f( x ) :
-    y = x + x**2 + x* np.exp(x)
+    y = 50*np.cos(10*math.sqrt(10)*x)
     return y
 
-x = np.linspace( 0, 1 )
+x = np.linspace( 0, 2 )
 y = f( x )
 plt.plot( x, y )
-plt.xlabel( 'x' )
-plt.ylabel( 'y' )
-plt.title( 'Plot of f(x,y)' )
+plt.xlabel( 't' )
+plt.ylabel( 'Q' )
+plt.title( 'Plot of Q(t)' )
 
 plt.text( 0.6, 0.3, 'Copyright @ 10927143' )
 plt.show( )
