748b99e2234c62260319e540457fa5f480dedd83
leo27628822 <leo27628822@gmail.com>
2023-03-31 14:22:30 +0800

    010

--- Python/Algorithm/10927141_1.py
+++ Python/Algorithm/10927141_1.py
@@ -8,6 +8,11 @@
 -2 1 -3 4 -1 2 1 -5
 16
 13 -3 -25 20 -3 -16 -23 18 20 -7 12 -5 -22 15 -4 7
+3
+1 1 1
+5
+4 3 2 1 1
+
 '''
 
 import numpy as np
@@ -16,13 +21,14 @@ import numpy as np
 #total_time = time.time() - start_time
 #print( total_time )
 
+'''
 def FindMaximumSubArray( A, low, high ):
-    if low == high :
+    if low == high-1 :
         return low, high, A[low]
     else :
         mid = ( low + high ) // 2
         left_low, left_high, left_sum = FindMaximumSubArray( A, low, mid )
-        right_low, right_high, right_sum = FindMaximumSubArray( A, mid+1, high )
+        right_low, right_high, right_sum = FindMaximumSubArray( A, mid, high )
         cross_low, cross_high, cross_sum = FindMaxCrossingSubArray( A, low, mid, high )
         if left_sum >= right_sum and left_sum >= cross_sum :
             return left_low, left_high, left_sum
@@ -32,26 +38,26 @@ def FindMaximumSubArray( A, low, high ):
             return cross_low, cross_high, cross_sum
 
 def FindMaxCrossingSubArray( A, low, mid, high ):
-    left_sum = -2147483648
+    left_sum = float('-inf')
     sum = 0
     max_left = mid
-    for i in range ( mid, low, -1 ) :
+    for i in range ( mid-1, low-1, -1 ) :
         sum += A[i]
         if sum > left_sum :
             left_sum = sum
             max_left = i
     
-    right_sum = -2147483648
+    right_sum = float('-inf')
     sum = 0
     max_right = mid + 1
-    for i in range ( mid+1, high ) :
+    for i in range ( mid, high ) :
         sum += A[i]
         if sum > right_sum :
             right_sum = sum
             max_right = i
 
     return max_left, max_right, left_sum + right_sum
-
+    '''
 def PrintArr( A, size ):
     n = 0
     while n != size:
@@ -60,6 +66,55 @@ def PrintArr( A, size ):
             print( " ", end="" )
         n += 1
     print()
+
+def Find_Maximum_Subarray( A ):
+	n = len( A )
+	low, high, sum = _Find_Maximum_Subarray( A, 0, n - 1 )
+	
+	print( "Maximum-Subarray Problem:" )
+	print( "Low =", low+1, "High =", high+1, "Sum =", sum )
+
+
+def _Find_Maximum_Subarray( A, low, high ):
+	if high == low:
+		return low, high, A[low]
+	else:
+		mid = ( low + high ) // 2 
+		
+		left_low, left_high, left_sum    = _Find_Maximum_Subarray( A, low, mid )
+		right_low, right_high, right_sum = _Find_Maximum_Subarray( A, mid + 1, high )		
+		cross_low, cross_high, cross_sum = _Find_Max_Crossing_Subarray( A, low, mid, high )
+		
+		if ( left_sum > right_sum and left_sum >= cross_sum ):
+			return left_low, left_high, left_sum
+		elif ( right_sum >= left_sum and right_sum >= cross_sum ):
+			return right_low, right_high, right_sum
+		else:
+			return cross_low, cross_high, cross_sum
+
+
+def _Find_Max_Crossing_Subarray( A, low, mid, high ):
+	left_sum = -3000000
+	sum = 0
+	for i in range( mid, low - 1, -1 ):
+		sum += A[i]
+		if sum > left_sum:
+			left_sum = sum		
+			max_left = i
+		
+	right_sum = -3000000
+	sum = 0
+	for j in range( mid + 1, high + 1 ):
+		sum += A[j]
+		if sum > right_sum:
+			right_sum = sum	
+			max_right = j
+	
+	cross_low  = max_left
+	cross_high = max_right
+	cross_sum  = left_sum + right_sum
+	
+	return cross_low, cross_high, cross_sum
 
 if __name__ == '__main__':
     
@@ -72,9 +127,10 @@ if __name__ == '__main__':
             continue
 
         A = np.array( input().split(), dtype=int )
-        low, high, sum = FindMaximumSubArray( A, 0, size-1 )
 
-        print( "Low = " + str( low+1 ), end = "" )
-        print( ", High = " + str( high+1 ), end = "" )
-        print( ", Sum = " + str( sum ), end = "\n" )
+        Find_Maximum_Subarray( A )
+
+        #print( "Low = " + str( low+1 ), end = "" )
+        #print( ", High = " + str( high+1 ), end = "" )
+        #print( ", Sum = " + str( sum ), end = "\n" )
         # PrintArr( A, size )
